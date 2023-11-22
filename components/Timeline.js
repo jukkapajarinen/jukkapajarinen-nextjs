80d8e089abc5510dd80a70fa5633f208064d65e5
Jukka Pajarinen <jukka.pajarinen@lumitum.com>
2023-11-23 00:11:33 +0200

    First commit

---
+++ components/Timeline.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Timeline = ({ entries }) => {
+  return (
+    <div className="mx-auto max-w-2xl">
+      {entries.map((entry, index) => (
+        <div key={index} className="mb-8">
+          <div className="flex items-center mb-4">
+            <div className="w-3 h-3 bg-blue-500 rounded-full mr-4"></div>
+            <div className="font-semibold">{entry.title}</div>
+          </div>
+          <div className="ml-7">
+            <p className="mb-2 text-gray-600">{entry.date}</p>
+            <p>{entry.description}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Timeline;
---
+++ pages/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { gql } from '@apollo/client';
+import client from '../lib/apolloClient';
+import Timeline from '../components/Timeline';
+
+const GET_TIMELINE_ENTRIES = gql`
+  query {
+    experience {
+      position
+      company
+      startDate
+      endDate
+      responsibilities
+    }
+  }
+`;
+
+const Home = ({ entries }) => {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <Timeline entries={entries} />
+    </div>
+  );
+};
+
+export const getStaticProps = async () => {
+  // const { data } = await client.query({
+  //   query: GET_TIMELINE_ENTRIES,
+  // });
+
+  // const entries = data.experience.map((experience) => ({
+  //   title: experience.position,
+  //   date: `${experience.startDate} - ${experience.endDate}`,
+  //   description: experience.responsibilities.join('\n'),
+  // }));
+
+  const entries = [];
+
+  return {
+    props: {
+      entries,
+    },
+  };
+};
+
+export default Home;
